e62c78be43a6ee512ea5078fd9a482950cc5f91e
chrisk <christian.khalil200@gmail.com>
2021-09-28 11:41:04 +0200

    Get Products Service with http client factory

---
+++ src/Congo.RazorPages/Services/IProductsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Congo.RazorPages.Models;
+
+namespace Congo.RazorPages.Services
+{
+    public interface IProductsService
+    {
+        Task<IEnumerable<Product>> GetProducts();
+    }
+}
