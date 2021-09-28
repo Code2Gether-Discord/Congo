e36d6a75bb45390d98822c165bf2422b1c2409eb
chrisk <christian.khalil200@gmail.com>
2021-09-28 18:57:58 +0200

    added dbcontext

---
+++ src/Congo.WebApi/Data/CongoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Congo.WebApi.Data
+{
+    public class CongoContext : DbContext
+    {
+        public CongoContext(DbContextOptions<CongoContext> options) : base(options)
+        {
+        }
+    }
+}
