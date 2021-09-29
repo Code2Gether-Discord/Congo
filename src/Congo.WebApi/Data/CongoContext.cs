27104509a848fb2d506aee5b825121153b03cce3
Ilia Iliev <ilia_iliev94@abv.bg>
2021-09-29 19:40:21 +0300

    Created the product model and added it as a dbset to the congo context.

--- src/Congo.WebApi/Data/CongoContext.cs
+++ src/Congo.WebApi/Data/CongoContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Congo.WebApi.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Congo.WebApi.Data
 {
     public class CongoContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
         public CongoContext(DbContextOptions<CongoContext> options) : base(options)
         {
         }
---
+++ src/Congo.WebApi/Data/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Congo.WebApi.Data.Models
+{
+    public class Product
+    {
+        [Key]
+        public string Id { get; init; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
