20d928726a09d3980b29f848bb02978a92d31a84
Palisar <83838480+Palisar@users.noreply.github.com>
2021-09-29 10:30:25 +0100

    Added ProductModel Using String for ID so we can add base64 encoding

---
+++ src/Congo.WebApi/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Congo.WebApi.Models
+{
+    public class ProductModel
+    {
+        public string Name{ get; set; }
+        public string ID { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImgUri { get; set; }
+    }
+}
