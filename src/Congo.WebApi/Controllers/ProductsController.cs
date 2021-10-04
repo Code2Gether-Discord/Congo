c86953b2eaee3cfd4168c6785c6650f8a6fb761d
Ilia Iliev <ilia_iliev94@abv.bg>
2021-10-04 18:57:42 +0300

    Added create method for products controller and created the product input model dto.

--- src/Congo.WebApi/Controllers/ProductsController.cs
+++ src/Congo.WebApi/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Congo.WebApi.Data;
 using Congo.WebApi.Data.Models;
 using Congo.WebApi.Data.ProductAccess;
+using Congo.WebApi.Models.Products;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -10,12 +11,12 @@ namespace Congo.WebApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class DemoProductController : Controller
+    public class ProductsController : Controller
     {
         // THIS IS PURELY FOR DEMO PURPOSES 
         private readonly IMediator _mediator;
 
-        public DemoProductController(IMediator mediator)
+        public ProductsController(IMediator mediator)
         {
            _mediator = mediator;
         }
@@ -24,6 +25,21 @@ namespace Congo.WebApi.Controllers
         public async Task<IEnumerable<Product>> Get()
         {
             return await _mediator.Send(new GetProductListQuery());
+        }
+
+        [HttpPost]
+        [Route("~/api/{seller}/products")]
+        public bool Create(ProductInputModel product)
+        {
+            if(!ModelState.IsValid)
+            {
+                return false;
+            }
+
+            _mediator.Send(new InsertProductCommand
+                (product.Name, product.Description, product.Price, product.ImageUrl));
+
+            return true;
         }
     }
 }
---
+++ src/Congo.WebApi/Models/Products/ProductInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Congo.WebApi.Models.Products
+{
+    public class ProductInputModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
