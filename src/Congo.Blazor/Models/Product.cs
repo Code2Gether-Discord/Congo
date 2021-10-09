c472a7ba9a81bdd6d8d5272beeee121ba68900c0
Vincent Ho <vinmaster99@gmail.com>
2021-10-09 00:09:42 -0700

    add blazor page for adding a product

---
+++ src/Congo.Blazor/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Congo.Blazor.Models
+{
+    public class Product
+    {
+        public Guid Id { get; init; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+
+}
