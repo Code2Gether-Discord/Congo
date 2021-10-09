386222d4fdea1a70f1339b5b9035586885791fb7
Palisar <Palisar@hotmail.co.uk>
2021-10-09 17:48:35 +0100

    Blazor Product Moded
    Added this so we can talk to the API via refit

---
+++ src/Congo.Blazor/Models/ProductModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Congo.Blazor.Models
+{
+    public class ProductModel
+    {
+        public string Name { get; init; }
+
+        public string Description { get; init; }
+
+        public decimal Price { get; init; }
+
+        public string ImageUrl { get; init; }
+    }
+}
