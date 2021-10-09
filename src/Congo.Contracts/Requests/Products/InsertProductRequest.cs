91f000cce7a607936005387351fd87754fa94181
Vincent Ho <vinmaster99@gmail.com>
2021-10-09 14:55:45 -0700

    use insert product request contract

--- src/Congo.Contracts/Requests/Products/InsertProductRequest.cs
+++ src/Congo.Contracts/Requests/Products/InsertProductRequest.cs
@@ -2,12 +2,12 @@
 {
     public sealed class InsertProductRequest
     {
-        public string Name { get; init; }
+        public string Name { get; set; }
 
-        public string Description { get; init; }
+        public string Description { get; set; }
 
-        public decimal Price { get; init; }
+        public decimal Price { get; set; }
 
-        public string ImageUrl { get; init; }
+        public string ImageUrl { get; set; }
     }
 }
