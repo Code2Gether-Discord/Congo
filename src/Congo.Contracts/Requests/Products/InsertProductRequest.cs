dcec0db0b86ef54f8a48188b772015186e643c0c
chrisK00 <72318023+chrisK00@users.noreply.github.com>
2021-10-09 18:30:08 +0200

    Refactor/50 move insert product request (#52)
    * Moved InsertProductRequest to Contracts
    
    * changed the seller controller's reference to InsertProductRequest

---
+++ src/Congo.Contracts/Requests/Products/InsertProductRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Congo.Contracts.Requests.Products
+{
+    public sealed class InsertProductRequest
+    {
+        public string Name { get; init; }
+
+        public string Description { get; init; }
+
+        public decimal Price { get; init; }
+
+        public string ImageUrl { get; init; }
+    }
+}
