3a89dd506799a38ad8fb7da4c17fba028d0a7588
Ilia Iliev <ilia_iliev94@abv.bg>
2021-10-10 16:25:06 +0300

    Added fluent validations and created an InsertProductRequestValidator class responsible for creating the validations for the InsertProductRequest model.

---
+++ src/Congo.WebApi/Validators/InsertProductRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Congo.Contracts.Requests.Products;
+using FluentValidation;
+
+namespace Congo.WebApi.Validators
+{
+    public class InsertProductRequestValidator : AbstractValidator<InsertProductRequest>
+    {
+        public InsertProductRequestValidator()
+        {
+            RuleFor(x => x.Name).NotNull();
+            RuleFor(x => x.Price).InclusiveBetween(0, decimal.MaxValue);
+        }
+    }
+}
