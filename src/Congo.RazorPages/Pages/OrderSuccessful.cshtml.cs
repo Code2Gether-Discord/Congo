1aed75331e18f26c762350775b37266a4f938d7f
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-10-11 14:04:56 -0500

    Add successful order page.

---
+++ src/Congo.RazorPages/Pages/OrderSuccessful.cshtml.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Congo.RazorPages.Pages
+{
+    public class OrderSuccessful : PageModel
+    {
+        [FromQuery(Name = "orderId")] public Guid OrderId { get; }
+
+        public void OnGet()
+        {
+        }
+    }
+}
