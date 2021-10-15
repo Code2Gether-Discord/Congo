713832c6912598dae6a88a86b8039853d678319c
chrisK00 <christian.khalil2000@gmail.com>
2021-10-15 14:17:14 +0200

    Added Refit Clients

---
+++ src/Congo.Contracts/Clients/ICongoUserClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Congo.Contracts.Responses.Orders;
+using Congo.Contracts.Responses.Products;
+using Refit;
+
+namespace Congo.Contracts.Clients
+{
+    public interface ICongoUserClient
+    {
+        [Get("/api/products")]
+        Task<ApiResponse<IEnumerable<ProductResponse>>> GetProducts();
+
+        [Post("/api/products/{id}/purchase")]
+        Task<ApiResponse<OrderConfirmationResponse>> PurchaseProduct(Guid id);
+    }
+}
