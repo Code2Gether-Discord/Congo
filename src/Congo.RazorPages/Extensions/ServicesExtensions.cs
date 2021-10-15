b766ca3ba19ee98251ca38c7fe3b6674739a995c
chrisK00 <christian.khalil2000@gmail.com>
2021-10-15 14:26:06 +0200

    added refit clients to front-end projects

--- src/Congo.RazorPages/Extensions/ServicesExtensions.cs
+++ src/Congo.RazorPages/Extensions/ServicesExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using Congo.Contracts.Clients;
 using Congo.RazorPages.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Refit;
 
 namespace Congo.RazorPages.Extensions
 {
@@ -17,6 +19,12 @@ namespace Congo.RazorPages.Extensions
             {
                 config.BaseAddress = new Uri("/api");
             });
+
+            services.AddRefitClient<ICongoUserClient>()
+                .ConfigureHttpClient(x => x.BaseAddress = new Uri("https://localhost:44388"));
+
+            services.AddRefitClient<ICongoSellerClient>()
+                .ConfigureHttpClient(x => x.BaseAddress = new Uri("https://localhost:44388"));
         }
     }
 }
