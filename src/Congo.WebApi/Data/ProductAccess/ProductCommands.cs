b5a7566c7bc417d9a2dcc1db91c11e2d9a5ce30c
chrisK00 <christian.khalil2000@gmail.com>
2021-10-24 11:31:43 +0200

    PurchaseProductHandler

--- src/Congo.WebApi/Data/ProductAccess/ProductCommands.cs
+++ src/Congo.WebApi/Data/ProductAccess/ProductCommands.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using Congo.Contracts.Responses.Orders;
 using MediatR;
 
 namespace Congo.WebApi.Data.ProductAccess
 {
     public record InsertProductCommand(string Name, string Description, decimal Price, string ImageUrl) : IRequest<Guid>;
+    public record PurchaseProductCommand(Guid Id) : IRequest<OrderConfirmationResponse>;
 }
