a787e56b5eed967661bc36efef5e780ea24b0b27
Chanakya <ucs3721@outlook.com>
2021-11-03 22:26:53 +0530

    Implement CORS for WebAPI project (#78)
    * Implement CORS for dev and prod
    - for dev, API can be accessed from any origin
    - for prod, env var `ALLOWED_ORIGIN` needs to be set
    
    * simplify `app.UseCors` middleware call and remove cors call from #77
    
    * code quality tweaks based on review: https://github.com/Code2Gether-Discord/Congo/pull/78#pullrequestreview-796653022
    
    * Update src/Congo.WebApi/Startup.cs
    
    Co-authored-by: Robin Rexstedt <robin.aakerman@gmail.com>
    
    Co-authored-by: Robin Rexstedt <robin.aakerman@gmail.com>

--- src/Congo.WebApi/Startup.cs
+++ src/Congo.WebApi/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Congo.WebApi.Extensions;
 using Congo.WebApi.Validators;
 using FluentValidation.AspNetCore;
@@ -13,6 +14,9 @@ namespace Congo.WebApi
 {
     public class Startup
     {
+        readonly string _CORS_DEV = "cors_dev";
+        readonly string _CORS_PROD = "cors_prod";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -24,8 +28,30 @@ namespace Congo.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAppServices(Configuration);
-
             services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(_CORS_DEV, policy =>
+                            policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+                options.AddPolicy(_CORS_PROD, policy =>
+                {
+                    // TODO: Multiple Origins from env vars
+                    string ALLOWED_ORIGIN = Environment.GetEnvironmentVariable("ALLOWED_ORIGIN");
+
+                    if (!string.IsNullOrWhiteSpace(ALLOWED_ORIGIN))
+                    {
+                        //Debug.WriteLine("ALLOWED_ORIGIN = " + ALLOWED_ORIGIN);
+                        policy.WithOrigins(ALLOWED_ORIGIN).AllowAnyMethod().AllowAnyHeader();
+                    }
+                    else if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") != "Development")
+                    {
+                        // Don't throw any exceptions in development, as this policy should only be used in production
+                        throw new Exception("No value set for mandatory environment variable ALLOWED_ORIGIN.");
+                    }
+                });
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Congo.WebApi", Version = "v1" });
@@ -37,17 +63,20 @@ namespace Congo.WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/middleware/?view=aspnetcore-5.0#middleware-order-1
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Congo.WebApi v1"));
-                app.UseCors(config => config.WithOrigins("*").AllowAnyMethod().AllowAnyHeader());
             }
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(env.IsDevelopment() ? _CORS_DEV : _CORS_PROD);
 
             app.UseAuthorization();
 
