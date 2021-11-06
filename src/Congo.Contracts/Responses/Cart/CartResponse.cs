c1a26d0e8381c3fa426ad999b18c013966f3cf4a
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-11-05 19:58:34 -0600

    add Cart and CartItem response models

---
+++ src/Congo.Contracts/Responses/Cart/CartResponse.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace Congo.Contracts.Responses.Cart
+{
+    public sealed class CartResponse
+    {
+        public Guid Id { get; init; }
+
+        public ICollection<CartItemResponse> CartItems { get; init; }
+
+        public decimal TotalPrice { get; init; }
+
+        public int Count { get; init; }
+    }
+}
