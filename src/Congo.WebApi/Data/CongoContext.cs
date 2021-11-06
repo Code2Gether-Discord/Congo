261da1e74e1194853eabb3a7480328669b015b02
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-11-06 17:22:24 -0600

    add Carts and CartItems to CongoContext

--- src/Congo.WebApi/Data/CongoContext.cs
+++ src/Congo.WebApi/Data/CongoContext.cs
@@ -13,6 +13,8 @@ namespace Congo.WebApi.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ src/Congo.WebApi/Data/Migrations/20211106222650_carts.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Congo.WebApi.Data.Migrations
+{
+    public partial class carts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Cart_CartId",
+                table: "CartItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Cart",
+                table: "Cart");
+
+            migrationBuilder.RenameTable(
+                name: "Cart",
+                newName: "Carts");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Carts",
+                table: "Carts",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems",
+                column: "CartId",
+                principalTable: "Carts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartItems_Carts_CartId",
+                table: "CartItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Carts",
+                table: "Carts");
+
+            migrationBuilder.RenameTable(
+                name: "Carts",
+                newName: "Cart");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Cart",
+                table: "Cart",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartItems_Cart_CartId",
+                table: "CartItems",
+                column: "CartId",
+                principalTable: "Cart",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
