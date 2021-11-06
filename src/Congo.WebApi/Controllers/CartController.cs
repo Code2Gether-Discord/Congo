b3f3a9b2c19af30fa0ffc878c92e6e9bf9215278
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-11-06 17:20:42 -0600

    add controller

---
+++ src/Congo.WebApi/Controllers/CartController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Congo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+    }
+}
