cf3a5969b2058e4fec2fb49cac313c0a9b459c41
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-11-06 22:31:37 -0600

    add controller logic

--- src/Congo.WebApi/Controllers/CartController.cs
+++ src/Congo.WebApi/Controllers/CartController.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Congo.Contracts.Responses.Cart;
+using Congo.WebApi.Data.CartAccess;
+using Mapster;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Congo.WebApi.Controllers
@@ -6,5 +13,23 @@ namespace Congo.WebApi.Controllers
     [ApiController]
     public class CartController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public CartController(IMediator mediator) => _mediator = mediator;
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CartResponse>>> Get()
+        {
+            var carts = await _mediator.Send(new GetCartsQuery());
+            return Ok(carts.Adapt<IEnumerable<CartResponse>>());
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<IEnumerable<CartItemResponse>>> Get(Guid id)
+        {
+            var cartItems = await _mediator.Send(new GetCartItemsQuery(id));
+            return Ok(cartItems.Adapt<IEnumerable<CartItemResponse>>());
+        }
     }
 }
