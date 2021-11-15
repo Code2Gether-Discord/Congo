28d00bf9d0f81c2a925947f1e104ea9790ad0110
Alex Barraza <diegobarrazapascual@hotmail.com>
2021-11-15 15:26:35 -0600

    Add Cart Endpoint (#81)
    * add Cart and CartItem response models
    
    * add controller
    
    * add Carts and CartItems to CongoContext
    
    * add CartQueries records and GetCartsHandler.cs
    
    * add GetCartItemsHandler
    
    * add mapping configurations
    
    * add controller logic
    
    * Add quantity to price calculation
    
    * Extract mapping config to its own class
    
    * Change ICollection to IReadOnlyCollection
    
    * Query just one cart
    
    * Remove GetCartsQuery, fix wrong return type on cartcontroller

---
+++ src/Congo.WebApi/Data/CartAccess/GetCartHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Congo.WebApi.Data.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Congo.WebApi.Data.CartAccess
+{
+    public class GetCartHandler : IRequestHandler<GetCartQuery, Cart>
+    {
+        private readonly CongoContext _dbContext;
+
+        public GetCartHandler(CongoContext dbContext) => _dbContext = dbContext;
+
+        public async Task<Cart> Handle(GetCartQuery request, CancellationToken cancellationToken)
+        {
+            var cart = await _dbContext.Carts.AsNoTracking()
+                .FirstOrDefaultAsync(c => c.Id == request.Id, cancellationToken);
+
+            _ = cart ?? throw new KeyNotFoundException($"Cart with id: {request.Id} was not found");
+            return cart;
+        }
+    }
+}
