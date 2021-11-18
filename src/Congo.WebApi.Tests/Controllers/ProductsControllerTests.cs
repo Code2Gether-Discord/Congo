1405fa422f7119bf47aae27ffe5238b1fd590143
Tim Fisicaro <tim.fisicaro@xenial.cloud>
2021-11-18 19:48:30 +0100

    test: :white_check_mark: mocked productscontroller test

---
+++ src/Congo.WebApi.Tests/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Congo.WebApi.Controllers;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace Congo.WebApi.Tests
+{
+    public class ProductsControllerTests
+    {
+        private readonly ProductsController sut;
+        private readonly Mock<IMediator> mediatorMoq;
+        private ProductsController _subject;
+
+        public ProductsControllerTests()
+        {
+            mediatorMoq = new Mock<IMediator>();
+            _subject = new ProductsController(mediatorMoq.Object);
+        }
+
+        [Fact]
+        public async void Should_ReturnListOfProductsWithStatus200()
+        {
+            // Arrange
+            var allProducts = await _subject.Get();
+
+            // Assert
+            allProducts.Result.Should().BeAssignableTo<OkObjectResult>();
+        }
+    }
+}
