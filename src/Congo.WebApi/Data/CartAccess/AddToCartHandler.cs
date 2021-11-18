842c7bb23ba4d833ca89876badb33e75776e8fb0
Palisar <palisar@hotmail.co.uk>
2021-11-18 16:57:04 +0000

    added Command and handler for adding to a Cart

---
+++ src/Congo.WebApi/Data/CartAccess/AddToCartHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Congo.WebApi.Data.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using static Congo.WebApi.Data.CartAccess.CartCommands;
+
+namespace Congo.WebApi.Data.CartAccess
+{
+    public class AddToCartHandler : IRequestHandler<AddToCartCommand, Guid>
+    {
+        private readonly CongoContext _dbContext;
+        private Cart cart;
+
+        public AddToCartHandler(CongoContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Guid> Handle(AddToCartCommand request, CancellationToken cancellationToken)
+        {
+            
+            if (request.cartId != null)
+            {
+                cart = await _dbContext.Carts.FirstOrDefaultAsync(c => c.Id == request.cartId);
+            }
+            else
+            {
+                cart = new Cart();
+            }
+                
+            var cartItem = new CartItem
+            {
+                Id = request.product.Id,
+                CartId = cart.Id,
+                Product = request.product,
+                Quantity = 1
+
+            };
+            await _dbContext.CartItems.AddAsync(cartItem);
+
+            await _dbContext.SaveChangesAsync();
+
+            return cart.Id;
+        }
+
+    }    
+}
---
+++ src/Congo.WebApi/Data/CartAccess/CartCommands.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Congo.WebApi.Data.Models;
+using MediatR;
+
+namespace Congo.WebApi.Data.CartAccess
+{
+    public class CartCommands
+    {
+        public record AddToCartCommand(Guid? cartId, Product product) : IRequest<Guid>;
+    }
+   
+}
