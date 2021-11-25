a3802356b8a67ae20788233cbd5bc7b407744efc
Palisar <palisar@hotmail.co.uk>
2021-11-25 21:23:20 +0000

    current WIP

--- src/Congo.WebApi/Data/CartAccess/AddToCartHandler.cs
+++ src/Congo.WebApi/Data/CartAccess/AddToCartHandler.cs
@@ -21,38 +21,56 @@ namespace Congo.WebApi.Data.CartAccess
 
         public async Task<Guid> Handle(AddToCartCommand request, CancellationToken cancellationToken)
         {
-            var cart = await _dbContext.Carts.FindAsync(request.cartId);
+            var cart = await _dbContext.Carts.FirstOrDefaultAsync(C => C.Id == request.cartId);
             var product = await _dbContext.Products.FindAsync(request.productId);
+            
             if (request.cartId != null && cart != null)
             {
                 _cart = cart;
             }
             else if (request.cartId == null)
             {
-                _cart = new Cart()
+                cart = new Cart()
                 {
                     Id = Guid.NewGuid(),
                 };
-                await _dbContext.Carts.AddAsync(_cart);
             }
             else return Guid.Empty;
+            // This sets the cart.
 
-            var cartItem = await AddCartItem(request, request.quantity);
-            await _dbContext.CartItems.AddAsync(cartItem);
+            var cartItem = await AddCartItem(request, cart, product);
+            
+            if (!await _dbContext.CartItems.ContainsAsync(cartItem))
+            {
+                cart.CartItems.Add(cartItem);
+                await _dbContext.CartItems.AddAsync(cartItem);
+            }
+            if (!await _dbContext.Carts.ContainsAsync(cart))
+            {
+                await _dbContext.Carts.AddAsync(cart);
+            }
+            
             _dbContext.SaveChanges();
 
-            return _cart.Id;
+            return cart.Id;
         }
-        private async Task<CartItem> AddCartItem(AddToCartCommand request, int quantity)
+        private async Task<CartItem> AddCartItem(AddToCartCommand request, Cart cart, Product product)
         {
+            foreach (CartItem item in cart.CartItems)
+            {
+                if (item.Product.Id == request.productId)
+                {
+                    item.Quantity = request.quantity;
+                    return item;
+                }
+            }
             return new CartItem
             {
                 Id = Guid.NewGuid(),
-                CartId = _cart.Id,
-                Product = await _dbContext.Products.FirstOrDefaultAsync(p => p.Id == request.productId),
+                CartId = cart.Id,
+                Product = product,
                 Quantity = request.quantity
             };
         }
-    }
-    
+    }    
 }
