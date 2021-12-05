5273428d925da3c0bc6f75336bdfc0ae2e071f22
Rich Dunne <richdunne@yahoo.com>
2021-12-05 16:17:46 -0700

    Initial commit

---
+++ src/Congo.Contracts/Requests/Cart/AddToCartRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Congo.WebApi.Requests.Cart
+{
+    public class AddToCartRequest
+    {
+        public Guid CartId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
